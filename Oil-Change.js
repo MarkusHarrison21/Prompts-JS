b95561293a5fc7ca51b96c47723713bd16611017
MarkusHarrison21 <79155269+MarkusHarrison21@users.noreply.github.com>
2021-02-24 14:05:55 -0500

    Add files via upload
    Coffee prompt HTML and JS. Oil Change prompt HTML and JS.

---
+++ Oil-Change.js
@@ -0,0 +1,12 @@
+change = Number
+var change = prompt("How many miles does your car have?");
+
+    if(change > 500) {
+        console.log("Yes I think I need an oil change."); 
+    }
+
+    else {
+        console.log("My car doesn't need an oil change"); 
+      }
+    
+    
